18db0d03a4b9ead55af9cb413298e617faf01c46
kushalmehta2004 <kushalmehta2004@gmail.com>
2025-06-02 12:56:11 +0530

    Fixed Tailwind CSS setup and verified backend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl">Indian Stock Trader</h1>
+    </div>
+  );
+}
+
+export default App;
